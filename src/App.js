708011b52f109b3e3106e6e431bb141909219c51
Aman Khare <113411805+Amkr1997@users.noreply.github.com>
2023-07-15 13:24:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import AddressBook from "./components/AddressBook";
+
+function App() {
+  return (
+    <Fragment>
+      <AddressBook />
+    </Fragment>
+  );
+}
+
+export default App;
